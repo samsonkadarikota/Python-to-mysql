4246e189f9fa542115e9aeb245822d003a883918
Kadarikota Samson <samsonkmoses50@gmail.com>
2025-07-16 15:25:02 +0530

    Add files via upload

---
+++ dataset3.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+conn = mysql.connector.connect(host = 'localhost',user='root',password='1478963')
+
+if conn.is_connected():
+    print('connection established')
+# print(conn)
+
+mycursor = conn.cursor()
+mycursor.execute('create database pythondb')
+print(mycursor)
